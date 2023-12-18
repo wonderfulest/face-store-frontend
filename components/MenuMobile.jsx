4b15541851313a5cab07e6b7acf4ab9575f83f57
muyids <289869876@qq.com>
2023-12-18 09:32:00 +0800

    add new products

--- components/MenuMobile.jsx
+++ components/MenuMobile.jsx
@@ -4,9 +4,9 @@ import { BsChevronDown } from "react-icons/bs";
 
 const data = [
     { id: 1, name: "Home", url: "/" },
-    { id: 2, name: "About", url: "/about" },
-    { id: 3, name: "Categories", subMenu: true },
-    { id: 4, name: "Contact", url: "/contact" },
+	{ id: 2, name: "Categories", subMenu: true },
+	{ id: 3, name: "FAQ", url: "/faq" },
+    // { id: 4, name: "Contact", url: "/contact" },
 ];
 
 const subMenuData = [
